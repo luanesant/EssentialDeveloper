93262fadee66035668afebda6835604f178de5e1
Luane dos Santos <luane.lopes@westwing.com.br>
2022-10-01 22:23:52 -0400

    Initial Commit

---
+++ EssentialDeveloper/Singleton.swift
@@ -0,0 +1,63 @@
+//
+//  Singleton.swift
+//  EssentialDeveloper
+//
+//  Created by Luane dos Santos Lopes on 01/10/22.
+//
+
+import UIKit
+
+/*
+ A classe ApiClient é compartilhada entre as Controllers de Login e Feed,
+ porém cada uma acessa uma determinada função, podendo então que a mesma (ApiClient)
+ estabeleça um tipo de dependência do código-fonte
+ */
+
+
+// Api Module
+class ApiClient{
+    static var shared = ApiClient()
+    
+    private init() {}
+    
+    func execute(_ : URLSession, completion: (Data) -> Void){}
+}
+
+//class MockApiClient: ApiClient{ }
+
+// Login Module
+
+struct LoggedInUser {}
+
+extension ApiClient{
+    func login(completion: (LoggedInUser) -> Void) {}
+}
+
+class LoginViewController: UIViewController{
+    let api = ApiClient.shared
+
+    func didTapLoginButton(){
+        api.login(){ user in
+            //Code
+        }
+    }
+}
+
+// Feed Module
+struct FeedItem {}
+
+extension ApiClient{
+    func loadFeed(completion: ([FeedItem]) -> Void) {}
+}
+
+class FeedViewController: UIViewController{
+    let api = ApiClient.shared
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        api.loadFeed{ loadedItems in
+            //Code
+        }
+    }
+}
---
+++ EssentialDeveloper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialDeveloper
+//
+//  Created by Luane dos Santos Lopes on 01/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
