064aebed2de85c00ec2720047b47e59aff609e3a
Luane dos Santos <luane.lopes@westwing.com.br>
2022-11-02 16:39:00 -0400

    Add fourth lesson is to connecting dots

---
+++ EssentialDeveloper/Lectures/Fourth/Connecting Dots.swift
@@ -0,0 +1,26 @@
+//
+//  Connecting Dots.swift
+//  EssentialDeveloper
+//
+//  Created by Luane dos Santos Lopes on 02/11/22.
+//
+
+import Foundation
+/** `Is relative to BDD, TDD Use Cases, requirements, architecture and modular design.`
+ English.
+ Ok, in the group example 1, we have the one big problem.
+ This BDD  isn't described correctly. Why? Because the dev (you) don't have all the necessary information, which ends up creating a lot of doubts.
+ 
+ 💬 By asking questions, developers can improve requirements and as a side effect build trust.
+ 
+ In the group example 2, we have a specifications to BDD and use cases, that help the dev (you) to have a step-by-step view of what should be done.
+ 
+ Portuguese:
+ Ok, na pasta de exemplo 1, temos um grande problema.
+ Este BDD não está descrito corretamente. Por quê? Porque o dev (você) não tem todas as informações necessárias,  o que acaba criando muitas dúvidas.
+ 
+ 💬 Ao fazer perguntas, os desenvolvedores podem melhorar os requisitos e como efeito colateral criar confiança.
+ 
+ Na pasta de  exemplo 2, temos uma especificação para BDD e casos de uso,que auxiliam o dev (você) a ter uma visão do passo-a-passo do que deve ser feito.
+
+ */
